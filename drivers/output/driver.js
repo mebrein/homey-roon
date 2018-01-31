206e415131da63a94e03e864b2f483f7d4c6b6be
Mebrein <mmourik@gmail.com>
2018-01-31 19:34:06 +0100

    url from roon extensions points to the Homey Apps repository

--- drivers/output/driver.js
+++ drivers/output/driver.js
@@ -48,7 +48,7 @@ class RoonOutputDriver extends Homey.Driver {
         publisher: 'Merijn van Mourik',
         email: 'mmourik@gmail.com',
         log_level: 'none',
-        website: 'https://github.com/mebrein/node-homey-roon',
+        website: 'https://apps.athom.com/app/nl.mebrein.homeyroon',
         get_persisted_state: () => (Homey.ManagerSettings.get('roonstate') || {}),
         set_persisted_state: state => {
           Homey.ManagerSettings.set('roonstate', state)
